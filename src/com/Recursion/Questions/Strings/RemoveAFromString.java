7b5e1c7c5af254185b372c0d965f22e4fc84f19a
RoHit4100 <rohitudamale777@gmail.com>
2023-09-09 12:31:48 +0530

    Recursion on the strings

--- src/com/Recursion/Questions/Strings/RemoveAFromString.java
+++ src/com/Recursion/Questions/Strings/RemoveAFromString.java
@@ -7,7 +7,7 @@ public class RemoveAFromString {
         skipOrModify(ans, str);
         System.out.println(skipOrModify(str));
     }
-
+    // In these we are using another string object and at the end of the recursion we are simply printing that ans
     static void skipOrModify(String newString, String givenString) {
         if (givenString.isEmpty()) {
             System.out.println(newString);
@@ -22,6 +22,8 @@ public class RemoveAFromString {
             skipOrModify(newString + ch, givenString.substring(1));
     }
 
+    // With return type and only with one argument.
+    // After completing recursion we are adding the current answer with its previous answer and finally returning that answer in the end
     static String skipOrModify(String string) {
         if (string.isEmpty()) {
             return "";
