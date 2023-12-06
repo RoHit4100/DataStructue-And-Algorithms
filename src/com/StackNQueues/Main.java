aa3457efc51a6661d1883f46b632e809a5945e74
RoHit4100 <rohitudamale777@gmail.com>
2023-12-06 20:17:55 +0530

    custom stack is implemented

--- src/com/StackNQueues/Main.java
+++ src/com/StackNQueues/Main.java
@@ -1,7 +1,15 @@
 package com.StackNQueues;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world!");
+        CustomStack stack = new CustomStack(10);
+        stack.add(19);
+        stack.add(99);
+        stack.add(8);
+        stack.add(1);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
     }
 }
